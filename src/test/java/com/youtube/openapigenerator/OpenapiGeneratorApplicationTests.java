e42ec7e7715b744d807104687d76faf4231bc275
Talha Imran <talhaimran284@gmail.com>
2023-04-15 14:36:46 +0500

    Initial Commit

---
+++ src/test/java/com/youtube/openapigenerator/OpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
